53243c0e3bdae5cbbed036a90dec762ebd21ab23
Vitor Cesario <vitorsousa676@gmail.com>
2023-11-09 13:16:09 -0300

    Rotas e atualizações

--- src/components/categoria/cardCategoria/CardCategoria.tsx
+++ src/components/categoria/cardCategoria/CardCategoria.tsx
@@ -1,37 +1,38 @@
-import { Link } from 'react-router-dom'
-import Categoria from '../../../models/Categoria'
+import { Link } from "react-router-dom";
+import Categoria from "../../../models/Categoria";
 
-interface CardCategoriaProps {
-    categoria: Categoria
+interface CardCategoriasProps {
+  categoria: Categoria;
 }
 
-function CardCategoria({ categoria }: CardCategoriaProps) {
-    return (
-        <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-            <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2xl'>
-                Categoria
-            </header>
+function CardCategorias({ categoria }: CardCategoriasProps) {
+  return (
+    <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
+      <header className="py-2 px-6 bg-teal-800 text-white font-bold text-2xl">
+        Categoria
+      </header>
 
-            <p className='p-8 text-3xl bg-slate-200 h-full'>
-                {categoria.nome}
-            </p>
+      <p className="p-8 text-3xl bg-slate-200 h-full">{categoria.nome}</p>
 
-            <div className="flex">
-                <Link to={`/editarCategorias/${categoria.id}`}
-                    className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
-                        flex items-center justify-center py-2'>
-                    <button>Editar</button>
-                </Link>
+      <div className="flex">
+        <Link
+          to={`/editarCategoria/${categoria.id}`}
+          className="w-full text-slate-100 bg-purple-700 hover:bg-purple-800 
+                        flex items-center justify-center py-2"
+        >
+          <button>Atualizar</button>
+        </Link>
 
-                <Link to={`/deletarCategorias/${categoria.id}`}
-                    className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                        flex items-center justify-center'>
-                    <button>Deletar</button>
-                </Link>
-            </div>
-
-        </div>
-    )
+        <Link
+          to={`/deletarCategoria/${categoria.id}`}
+          className="text-slate-100 bg-red-600 hover:bg-red-700 w-full 
+                        flex items-center justify-center"
+        >
+          <button>Deletar</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default CardCategoria
+export default CardCategorias;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,33 @@
 
-function Navbar() {
-    return (
-        <>
-            <div className='w-full bg-indigo-900 text-white
-                flex justify-center py-4'>
-            
-                <div className="container flex justify-between text-lg">
-                    Projeto Farmacia
+import { Link } from "react-router-dom";
 
-                    <div className='flex gap-4'>
-                       Pordutos
-                       Categorias
-                       Cadastrar Categorias
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+function Navbar() {
+  return (
+    <>
+      <div
+        className="w-full bg-indigo-900 text-white
+                flex justify-center py-4"
+      >
+        <div className="container flex justify-between text-lg">
+          Projeto Farmacia
+          <div className="flex gap-4">
+            <Link to="/categorias" className="hover:underline">
+              Categorias
+            </Link>
+            <Link to="/cadastrarCategoria" className="hover:underline">
+              Cadastrar
+            </Link>
+            <Link to="/editarCategoria" className="hover:underline">
+              Atualizar
+            </Link>
+            <Link to="/deletarCategoria" className="hover:underline">
+              Deletar
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
